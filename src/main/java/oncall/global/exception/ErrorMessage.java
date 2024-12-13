7541748419c9651558cfd8c89b54c3a26d610f99
joungeun0825 <joungeun0825@gmail.com>
2024-12-13 10:28:41 +0900

    feat: 에러 처리 클래스 작성

---
+++ src/main/java/oncall/global/exception/ErrorMessage.java
@@ -0,0 +1,15 @@
+package oncall.global.exception;
+
+public enum ErrorMessage {
+    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
+    INVALID_INPUT_ERROR("유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
